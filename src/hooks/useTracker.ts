80096d173790e4733385bda49966dfd656608533
sumanwmit <suman.wmit@gmail.com>
2026-08-13 09:45:32 +0530

    Update useTracker.ts

--- src/hooks/useTracker.ts
+++ src/hooks/useTracker.ts
@@ -5,7 +5,7 @@ export function useTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
 
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
